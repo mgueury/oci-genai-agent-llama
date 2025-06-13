ebe84c3410df31bd8344f67a2fd1c201793d2dab
Marc Gueury <marc.gueury@oracle.com>
2025-06-13 22:04:23 +0200

    v3.0

--- src/app/src/install.sh
+++ src/app/src/install.sh
@@ -20,7 +20,7 @@ sudo firewall-cmd --reload
 
 # Configure APEX settings
 export TNS_ADMIN=$HOME/db
-$HOME/db/sqlcl/bin/sql APEX_APP/$DB_PASSWORD@DB << EOF
+$HOME/db/sqlcl/bin/sql $DB_USER/$DB_PASSWORD@DB << EOF
 begin
   update APEX_APP.AI_CONFIG set value='$TF_VAR_compartment_ocid' where key='compartment_ocid';
   update APEX_APP.AI_CONFIG set value='https://$APIGW_HOSTNAME/app/chat' where key='qa_url';
